c28c4ac37953fa0d9a1214fce75cdbca78a7f3c5
rem@archbox <crimsonsky.rem@gmail.com>
2017-01-10 23:40:26 +0800

    add amazeui

--- views/memo.jsx
+++ views/memo.jsx
@@ -1,13 +1,35 @@
 var React = require('react');
+import Perf from 'react-addons-test-utils';
+import {Container,Group,List} from 'amazeui-touch';
 
+//class Memo extends React.Component {
+//    render() {
+//        const listItems = this.props.data.map((value)=>
+//            <li key={value[0]}>{value[1]}</li>);
+//        return (
+//            <ul>
+//                {listItems}
+//            </ul>);
+//    }
+//}
 class Memo extends React.Component {
-    render() {
-        const listItems = this.props.data.map((value)=>
-            <li key={value[0]}>{value[1]}</li>);
+    render(){
         return (
-            <ul>
-                {listItems}
-            </ul>);
+            <Container>
+                <Group
+                    header="备忘"
+                >
+                    <List>
+                        {this.props.data.map((value)=>{
+                            return (<List.Item
+                                title={value[1]}
+                                key={value[0]}
+                            />);
+                        })}
+                    </List>
+                </Group>
+            </Container>
+        );
     }
 }
 
