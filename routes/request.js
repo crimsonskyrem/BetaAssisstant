fd8d518b07d830982dfc236ff3094537ff04f639
rem@archbox <crimsonsky.rem@gmail.com>
2017-02-03 13:05:26 +0800

    --amend

---
+++ routes/request.js
@@ -0,0 +1,16 @@
+av = require('leanengine');
+
+exports.request = function(req, res){
+    const usr = req.body.usr;
+    const view = req.body.view=='TODO'?'todoList':'memoList';
+    const que = new av.Query(view);
+    que.equalTo('userId',usr);
+    que.addDescending('updatedAt');
+    que.find().then(function(results){
+        var arr = [];
+        for(var i=0;i<results.length;i++){
+            arr[i] = {subtitle:results[i].get('content'),title:results[i].getCreatedAt()};
+        }
+        res.json(arr);
+    });
+};
