7e4b75a7d3651d4f8a114ff81e3051537eb35acd
rem@archbox <crimsonsky.rem@gmail.com>
2017-02-12 19:39:09 +0800

    remove todos staggermotion

--- public/app/components/TodoView.jsx
+++ public/app/components/TodoView.jsx
@@ -5,29 +5,36 @@ import {StaggeredMotion,spring,presets} from 'react-motion';
 class TodoView extends Component{
     render(){
         const {data,onToggleTodo,onDeleteTodo,onSwipeTodoTab} = this.props;
-        return (
-        <StaggeredMotion
-        defaultStyles={data.map((_,i)=>({left:-1000}))}
-        styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
-            return i === 0
-            ? {left: spring(0)}
-            : {left: spring(prevInterpolatedStyles[i - 1].left)}
-        })}>
-        {interpolatingStyles =>
-            <div>
-            {interpolatingStyles.map((style, i) =>
-                <div key={i} style={{position:'relative',left: style.left}}>
+        const Lists = data.map(item =>
                     <TodoItemView
-                        value={data[i]}
+                        value={item}
                         onSwipeTodoTab={onSwipeTodoTab}
                         onToggleTodo={onToggleTodo}
-                        onDeleteTodo={onDeleteTodo} />
-                </div>)
-            }
-            </div>
-        }
-        </StaggeredMotion>
-       );
+                        onDeleteTodo={onDeleteTodo} />);
+        return <div>{Lists}</div>
+//        return (
+//        <StaggeredMotion
+//        defaultStyles={data.map((_,i)=>({left:-1000}))}
+//        styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+//            return i === 0
+//            ? {left: spring(0)}
+//            : {left: spring(prevInterpolatedStyles[i - 1].left)}
+//        })}>
+//        {interpolatingStyles =>
+//            <div>
+//            {interpolatingStyles.map((style, i) =>
+//                <div key={i} style={{position:'relative',left: style.left}}>
+//                    <TodoItemView
+//                        value={data[i]}
+//                        onSwipeTodoTab={onSwipeTodoTab}
+//                        onToggleTodo={onToggleTodo}
+//                        onDeleteTodo={onDeleteTodo} />
+//                </div>)
+//            }
+//            </div>
+//        }
+//        </StaggeredMotion>
+//       );
     }
 }
 export default TodoView;
